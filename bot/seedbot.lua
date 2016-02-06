85d96eb94f9b40f730be71ee2cda3ff56903b912
sini370 <sina.e.1380@gmail.com>
2016-02-06 18:21:49 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -224,7 +224,8 @@ function create_config( )
     "invite",
     "all",
     "leave_ban",
-    "admin"
+    "admin",
+    "plugins"
     },
     sudo_users = {112274576,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
