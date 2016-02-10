61eea541f145cfc2b98b734bbaa92b9281b10769
sini370 <sina.e.1380@gmail.com>
2016-02-10 15:09:56 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -227,29 +227,21 @@ function create_config( )
     "admin",
     "plugins"
     },
-    sudo_users = {112274576,0,tonumber(our_id)},--Sudo users
+    sudo_users = {112274576,(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v2 - Open Source
-An advance Administration bot based on yagop/telegram-bot 
-
-https://github.com/SEEDTEAM/TeleSeed
-
-Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@Rondoozle [Developer]
-@seyedan25 [Manager]
-
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
-
-Our channels
-@teleseedch [English]
-@iranseed [persian]
+    about_text = [[
+  Channel: @telelife_ch
+  Sudo (Admin): @mrlife
+  
+  Powered by:
+  telelifeGroup
+  Engineer sina ebrahimi
+tnx to
+@seydan25
+@imandaneshi
+@iwals
+]
 ]],
     help_text_realm = [[
 Realm Commands:
